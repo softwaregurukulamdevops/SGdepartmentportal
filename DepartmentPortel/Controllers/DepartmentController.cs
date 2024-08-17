ad5ca83b37013b5e03a25f2a804ab87614bfc1a2
Kotagiri lokesh <ktss.kotagiri@gmail.com>
2024-08-17 21:14:49 +0530

    Departmentportal

---
+++ DepartmentPortel/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using DepartmentPortel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentPortel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        public readonly TrainingDBContext trainingDBContext;
+        public DepartmentController(TrainingDBContext _trainingDBContext)
+        {
+            trainingDBContext = _trainingDBContext;
+        }
+        [HttpGet("GetDepartmentDetails")]
+        public List<Department> GetDepartmentDetails()
+        {
+            List<Department> lstDepartment = new List<Department>();
+            try
+            {
+                lstDepartment = trainingDBContext.Department.ToList();
+                return lstDepartment;
+            }
+            catch (Exception ex)
+            {
+                lstDepartment = new List<Department>();
+                return lstDepartment;
+            }
+        }
+        [HttpPost("AddDepartment")]
+        public string AddDepartmentDetails(Department department)
+        {
+            string message = string.Empty;
+            try
+            {
+                if (!string.IsNullOrEmpty(department.DepartmentName))
+                {
+                    trainingDBContext.Add(department);
+                    trainingDBContext.SaveChanges();
+                    message = "Department added successfully";
+                }
+                else
+                    message = "Description required";
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return message;
+        }
+    }
+}
---
+++ DepartmentPortel/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DepartmentPortel.Models
+{
+    public partial class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        [StringLength(100)]
+        public string DepartmentName { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
